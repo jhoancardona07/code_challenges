fe585905b04dd942f45ae66b44fc33ad1cd539b6
Jorge <j.l-h@hotmail.com>
2020-08-15 20:03:43 -0500

    Solved exercise

---
+++ validateBalancedParenthesis/JLH.py
@@ -0,0 +1,28 @@
+class Solution:
+  def isValid(self, string):
+    stack=[]
+    for character in string:
+        if(character == "(" or character=="[" or character=="{"):
+            stack.append(character)
+        if(character == ")"):
+            if(not(stack.pop()=="(")):
+                stack.append("(")
+        if(character == "]"):
+            if(not(stack.pop()=="[")):
+                stack.append("[")
+        if(character == "}"):
+            if(not(stack.pop()=="{")):
+                stack.append("{")    
+    if(len(stack)==0):
+        return True
+    else:
+        return False
+
+s="((()))"
+print(s, Solution().isValid(s))
+s="[()]{}"
+print(s, Solution().isValid(s))
+s="({[)]"
+print(s, Solution().isValid(s))
+s=""
+print(s, Solution().isValid(s))
